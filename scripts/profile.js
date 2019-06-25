941bd530fca3521c7b12a92ca9e3ca930eb4b1c3
Joneeh <joniseluryrasco@gmail.com>
2019-06-26 02:44:39 +0800

    upload

---
+++ scripts/profile.js
@@ -0,0 +1,9 @@
+
+function show_hide(){
+			var click = document.getElementsById("dropdown-content");
+			if (click.style.display === "none"){
+				click.style.display="block";
+				}else{
+					click.style.display="none"
+				}
+}
